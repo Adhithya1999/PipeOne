dbd5127c48dc1ee05980f3edc7f23ea937331733
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-26 16:24:20 +0530

    added script

--- Pipeone.java
+++ Pipeone.java
@@ -1,9 +1,30 @@
-public class Pipeone {
-
- public static void main(String[] args) {
-  // TODO Auto-generated method stub
-    System.out.println("first pipe is constructed");
-
- }
-
+pipeline{
+    agent any
+    stages{
+        stage('Compile'){
+            steps{
+                echo"compiling the script"
+            }
+        }
+        stage('Build'){
+            steps{
+                echo"building the script"
+            }
+        }
+        stage('Deploy'){
+            steps{
+                echo"Deploying the script"
+            }
+        }
+        stage('Release'){
+            steps{
+                echo"releasing the script"
+            }
+        }
+        stage('summa'){
+            steps{
+                echo"summa poten"
+            }
+        }
+    }
 }
